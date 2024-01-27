dbe352a6968ef92fe9388268c9218ea8ce8aebbf
Saadet Çatak Akgüneş <saadetcatak1453@gmail.com>
2024-01-27 23:48:19 +0300

    update

--- Services/Order/Presentation/MyAkademiECommerce.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MyAkademiECommerce.Order.WebApi/Program.cs
@@ -1,6 +1,21 @@
+using MyAkademiECommerce.Order.Application.Features.CQRS.Handlers;
+using MyAkademiECommerce.Order.Application.Interfaces;
+using MyAkademiECommerce.Order.Persistence.Context;
+using MyAkademiECommerce.Order.Persistence.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+});
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddDbContext<OrderContext>();
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
